84589ab3d038a1437dbbe884c61f4d32d3ed9d0c
RickBodewes <Rick@planet.nl>
2021-04-16 21:32:15 +0200

    add glitch effect base and header base

--- index.php
+++ index.php
@@ -6,7 +6,14 @@
     </head>
     <body>
         <div id="wrapper">
-            
+            <section class="full-height" id="header_home">
+                <div id="header_home_canvas_container"></div>
+                <div id="header_home_content_container">
+                    <div id="header_home_title">
+                        <h1 class="glitch" data-text="Rick Bodewes">Rick Bodewes</h1>
+                    </div>
+                </div>
+            </section><!-- header -->
         </div><!-- wrapper -->
     </body>
 </html>
