8ca3763c44da384252089adc4292cf62e55fde04
RickBodewes <Rick@planet.nl>
2021-04-16 18:46:15 +0200

    set up main css tree

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Rick Bodewes</title>
+        <?php require "include/header.html" ?>
+    </head>
+    <body>
+        <div id="wrapper">
+            
+        </div><!-- wrapper -->
+    </body>
+</html>
